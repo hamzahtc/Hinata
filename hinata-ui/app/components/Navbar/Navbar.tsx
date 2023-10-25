b787b0245305dd1eb02b6f3365c445eb7d99dcf5
hamzahtc <hamzahatoch04@gmail.com>
2023-10-25 22:43:53 +0100

    :sparkles: add theme config

--- hinata-ui/app/components/Navbar/Navbar.tsx
+++ hinata-ui/app/components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 
 const Navbar = () => {
   return (
@@ -12,7 +11,6 @@ const Navbar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Hinata parle pas chinois
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
