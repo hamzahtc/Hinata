ca42edd1cd7363eac01e66a8f95690a1a434f49b
hamzahtc <hamzahatoch04@gmail.com>
2023-10-27 19:06:30 +0100

    change content

--- hinata-ui/app/components/Blogs/Blogs.tsx
+++ hinata-ui/app/components/Blogs/Blogs.tsx
@@ -3,7 +3,7 @@ import { Stack, Typography } from "@mui/material";
 const Blogs = () => {
   return (
     <Stack alignItems="center" sx={{ p: 10 }}>
-      <Typography fontSize={30}>Hinata is the best</Typography>
+      <Typography fontSize={30}>Hinata parle pas chinois</Typography>
     </Stack>
   );
 };
