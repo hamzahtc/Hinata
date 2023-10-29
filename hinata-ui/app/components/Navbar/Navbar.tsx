ab43446f0c4b3d9f7000083cc3daa271e75f10e3
hamzahtc <36455639+hamzahtc@users.noreply.github.com>
2023-10-29 01:06:27 +0100

    Update Navbar.tsx

--- hinata-ui/app/components/Navbar/Navbar.tsx
+++ hinata-ui/app/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Hina-ta
+            Hinata
           </Typography>
         </Toolbar>
       </AppBar>
